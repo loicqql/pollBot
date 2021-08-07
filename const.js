8ed3e7d4c309c24b9bf25ed816dd0cc968e149b2
loic quinquenel <loic.qql@gmail.com>
2021-08-07 20:54:54 +0200

    fix: custom prefix

--- const.js
+++ const.js
@@ -1,4 +1,4 @@
 export const IMAGE = process.env.IMAGE;
 export const COULEUR = process.env.COULEUR;
 export const NUMBER = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
-export const PREFIX = '!poll';
+export const PREFIX = process.env.PREFIX;
